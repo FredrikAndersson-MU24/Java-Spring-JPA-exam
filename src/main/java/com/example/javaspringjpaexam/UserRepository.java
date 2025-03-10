87452d0dfe0feef658d11aeeb1bbd772ba703bfe
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-03-10 15:14:36 +0100

    Add method to search for name containing passed string

--- src/main/java/com/example/javaspringjpaexam/UserRepository.java
+++ src/main/java/com/example/javaspringjpaexam/UserRepository.java
@@ -3,6 +3,11 @@ package com.example.javaspringjpaexam;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    List<User> findUsersByNameContainingIgnoreCase(String name);
+
 }
