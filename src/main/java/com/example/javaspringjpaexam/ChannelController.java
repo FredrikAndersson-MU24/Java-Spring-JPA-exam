a2d5c136ac15cef98b6ba50ea986de9a998da268
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-03-10 17:26:40 +0100

    Add endpoint to create Channel

--- src/main/java/com/example/javaspringjpaexam/ChannelController.java
+++ src/main/java/com/example/javaspringjpaexam/ChannelController.java
@@ -1,19 +1,27 @@
 package com.example.javaspringjpaexam;
 
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/channel")
+@RequestMapping("/channels")
 public class ChannelController {
 
     private ChannelService channelService;
 
-    public ChannelService getChannelService() {
-        return channelService;
-    }
-
-    public void setChannelService(ChannelService channelService) {
+    public ChannelController(ChannelService channelService) {
         this.channelService = channelService;
     }
+
+
+
+    @PostMapping
+    public ResponseEntity<Channel> createChannel(@RequestBody @Valid Channel newChannel) {
+        return ResponseEntity.ok(channelService.createChannel(newChannel));
+    }
+
 }
