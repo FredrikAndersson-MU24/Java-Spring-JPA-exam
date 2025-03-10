e722e8977292b06725386e06bb1f1179cd97579e
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-03-10 20:41:31 +0100

    Add method to get channels by free text search

--- src/main/java/com/example/javaspringjpaexam/ChannelRepository.java
+++ src/main/java/com/example/javaspringjpaexam/ChannelRepository.java
@@ -3,6 +3,10 @@ package com.example.javaspringjpaexam;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ChannelRepository extends JpaRepository<Channel, Long> {
+
+    List<Channel> findChannelsByNameContainingIgnoreCase(String title);
 }
