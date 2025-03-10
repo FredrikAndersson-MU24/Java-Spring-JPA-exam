ea296141e7d84ecc7d46e414297b70cf94813513
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-03-10 15:15:17 +0100

    Add endpoints to get users from db

--- src/main/java/com/example/javaspringjpaexam/UserController.java
+++ src/main/java/com/example/javaspringjpaexam/UserController.java
@@ -1,11 +1,11 @@
 package com.example.javaspringjpaexam;
 
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -20,6 +20,24 @@ public class UserController {
     @PostMapping
     public ResponseEntity<User> addUser(@RequestBody @Valid User user) {
         return ResponseEntity.ok(userService.addUser(user));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @GetMapping("/{id}/id")
+    public ResponseEntity<User> getUserById(@PathVariable long id) {
+        User user = userService.getUserById(id);
+        if ( user != null){
+            return ResponseEntity.ok(user);
+        } else return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/name/{name}")
+    public ResponseEntity<List<User>> getUsersByName(@PathVariable String name) {
+        return ResponseEntity.ok(userService.getUsersByName(name));
     }
 
 
