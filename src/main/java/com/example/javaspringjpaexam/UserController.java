9a591451b6cd50c2cfe3e1dc3c5496031a6404e4
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-03-10 14:56:20 +0100

    Add endpoint to add user

--- src/main/java/com/example/javaspringjpaexam/UserController.java
+++ src/main/java/com/example/javaspringjpaexam/UserController.java
@@ -1,5 +1,8 @@
 package com.example.javaspringjpaexam;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +15,12 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
+
+    @PostMapping
+    public ResponseEntity<User> addUser(@RequestBody User user) {
+        return ResponseEntity.ok(userService.addUser(user));
+    }
+
+
+
 }
