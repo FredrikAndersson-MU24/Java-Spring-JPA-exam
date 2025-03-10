e09b308838cb68e31b2d53e3a2cf373a8c34669a
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-03-10 17:37:52 +0100

    Add method to get all channels

--- src/main/java/com/example/javaspringjpaexam/ChannelService.java
+++ src/main/java/com/example/javaspringjpaexam/ChannelService.java
@@ -2,6 +2,8 @@ package com.example.javaspringjpaexam;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ChannelService {
 
@@ -9,6 +11,10 @@ public class ChannelService {
 
     public ChannelService(ChannelRepository channelRepository) {
         this.channelRepository = channelRepository;
+    }
+
+    public List<Channel> getAllChannels() {
+        return channelRepository.findAll();
     }
 
     public Channel createChannel(Channel newChannel) {
