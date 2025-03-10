311713dbe7f2fa9a44223ae11f61cf8fcb85632e
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-03-10 13:42:48 +0100

    First commit

---
+++ src/main/java/com/example/javaspringjpaexam/JavaSpringJpaExamApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaspringjpaexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringJpaExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringJpaExamApplication.class, args);
+    }
+
+}
