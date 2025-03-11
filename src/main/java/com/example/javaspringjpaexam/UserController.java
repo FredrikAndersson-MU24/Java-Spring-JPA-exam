6528368f1303a8197fef35449c6b37576493270c
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-03-11 10:19:19 +0100

    Add endpoints to create read posts on user id

--- src/main/java/com/example/javaspringjpaexam/UserController.java
+++ src/main/java/com/example/javaspringjpaexam/UserController.java
@@ -18,17 +18,27 @@ public class UserController {
         this.userService = userService;
     }
 
+    //Create
     @PostMapping
     public ResponseEntity<User> addUser(@RequestBody @Valid User user) {
         return ResponseEntity.ok(userService.addUser(user));
     }
 
+    @PostMapping("/posts/{userId}")
+    public ResponseEntity<Post> createPostOnUserId(@RequestBody @Valid Post newPost, @PathVariable long userId) {
+        Post post = userService.createPostOnUserId(newPost, userId);
+        if ( post != null){
+            return ResponseEntity.ok(post);
+        } else return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    //Read
     @GetMapping
     public ResponseEntity<List<User>> getAllUsers() {
         return ResponseEntity.ok(userService.getAllUsers());
     }
 
-    @GetMapping("/{id}/id")
+    @GetMapping("/{id}")
     public ResponseEntity<User> getUserById(@PathVariable long id) {
         User user = userService.getUserById(id);
         if ( user != null){
@@ -41,15 +51,22 @@ public class UserController {
         return ResponseEntity.ok(userService.getUsersByName(name));
     }
 
-    @PutMapping("/{userId}/id")
-    public ResponseEntity<User> updateUserById (@RequestBody @Valid User userToUpdate, @PathVariable long userId) {
-        User user = userService.updateUserById(userToUpdate, userId);
+    @GetMapping("/posts/{userId}")
+    public ResponseEntity<List<Post>> getUsersPosts(@PathVariable long userId) {
+        return ResponseEntity.ok(userService.getUsersPosts(userId));
+    }
+
+    //Update
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUserById (@RequestBody @Valid User userToUpdate, @PathVariable long id) {
+        User user = userService.updateUserById(userToUpdate, id);
         if ( user != null){
             return ResponseEntity.ok(user);
         } else return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    @DeleteMapping("/{id}/id")
+    //Delete
+    @DeleteMapping("/{id}")
     public void deleteUserById(@PathVariable  long id) throws BadRequestException {
         userService.deleteUser(id);
     }
