125e656cd6cdc409343e2342988b394e9fc9413d
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-03-11 10:22:16 +0100

    Add method to find posts by user id

--- src/main/java/com/example/javaspringjpaexam/PostRepository.java
+++ src/main/java/com/example/javaspringjpaexam/PostRepository.java
@@ -3,6 +3,11 @@ package com.example.javaspringjpaexam;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    List<Post> findAllByUser_Id(long userId);
+
 }
