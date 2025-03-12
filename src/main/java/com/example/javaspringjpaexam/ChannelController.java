7540cbffff2a9632b7fa61651d211a2ef2b9c606
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-03-12 15:30:17 +0100

    Remove unused argument

--- src/main/java/com/example/javaspringjpaexam/ChannelController.java
+++ src/main/java/com/example/javaspringjpaexam/ChannelController.java
@@ -14,7 +14,7 @@ public class ChannelController {
 
     private final ChannelService channelService;
 
-    public ChannelController(ChannelService channelService, UserService userService) {
+    public ChannelController(ChannelService channelService) {
         this.channelService = channelService;
     }
 
