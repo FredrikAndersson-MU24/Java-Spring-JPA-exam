aec40938d10f2116d869a9a97e39392e19e78524
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-03-12 20:07:51 +0100

    Adapt to return DTOs

--- src/main/java/com/example/javaspringjpaexam/ChannelService.java
+++ src/main/java/com/example/javaspringjpaexam/ChannelService.java
@@ -18,8 +18,8 @@ public class ChannelService {
     }
 
     // Create
-    public Channel createChannel(Channel newChannel) {
-        return channelRepository.save(newChannel);
+    public ChannelDTO createChannel(Channel newChannel) {
+        return ChannelMapper.INSTANCE.channelToChannelDTO(channelRepository.save(newChannel));
     }
 
     //Read
@@ -27,32 +27,39 @@ public class ChannelService {
         return channelRepository.findAll().stream().map(ChannelMapper.INSTANCE::channelToChannelDTO).collect(Collectors.toList());
     }
 
-    public Channel getChannelById(long id) {
-        return channelRepository.findById(id).orElse(null);
+    public ChannelDTO getChannelById(long id) {
+        return ChannelMapper.INSTANCE.channelToChannelDTO(channelRepository.findById(id).orElse(null));
     }
 
-    public List<Channel> getChannelsByFreeText(String searchTerm) {
-        return channelRepository.findChannelsByNameContainingIgnoreCase(searchTerm);
+    public List<ChannelDTO> getChannelsByFreeText(String searchTerm) {
+        List<Channel> channels = channelRepository.findChannelsByNameContainingIgnoreCase(searchTerm);
+
+        return channels.stream().map(ChannelMapper.INSTANCE::channelToChannelDTO).toList();
     }
 
-    public List<Post> getAllPostsByChannelId(long channelId) {
-        return postRepository.findAllByChannel_Id(channelId);
+    public List<PostDetailedDTO> getAllPostDetailedDTOByChannelId(long channelId) {
+        List<Post> posts = postRepository.findAllByChannel_Id(channelId);
+        return posts.stream().map(PostMapper.INSTANCE::postToPostDetailedDTO).toList();
+    }
+
+    public List<PostMinimalDTO> getAllPostMinimalDTOByChannelId(long channelId) {
+        List<Post> posts = postRepository.findAllByChannel_Id(channelId);
+        return posts.stream().map(PostMapper.INSTANCE::postToPostMinimalDTO).toList();
     }
 
     //Update
-    public Channel updateChannelByID(Channel channelToUpdate, long id) {
-        Channel channel = getChannelById(id);
+    public ChannelDTO updateChannelByID(Channel channelToUpdate, long id) {
+        Channel channel = channelRepository.findById(id).orElse(null);
         if (channel != null) {
             channel.setName(channelToUpdate.getName());
-            return channelRepository.save(channel);
+            return ChannelMapper.INSTANCE.channelToChannelDTO(channelRepository.save(channel));
         }
         return null;
     }
 
-
     //Delete
     public void deleteChannelById(long id) throws BadRequestException {
-        if(getChannelById(id) != null){
+        if (getChannelById(id) != null) {
             channelRepository.deleteById(id);
         } else throw new BadRequestException("Channel not found");
     }
