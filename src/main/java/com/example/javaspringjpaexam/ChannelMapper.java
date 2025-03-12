670445505f876bf4db8883877e9175970ed2a3ea
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-03-12 15:27:30 +0100

    Count number of posts for Channel to ChannelDTO

--- src/main/java/com/example/javaspringjpaexam/ChannelMapper.java
+++ src/main/java/com/example/javaspringjpaexam/ChannelMapper.java
@@ -1,12 +1,21 @@
 package com.example.javaspringjpaexam;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
+
+import java.util.List;
 
 @Mapper
 public interface ChannelMapper {
 
     ChannelMapper INSTANCE = Mappers.getMapper(ChannelMapper.class);
 
+    @Mapping(expression = "java(getNumberOfPosts(channel.getPosts()))", target ="postCount")
     ChannelDTO channelToChannelDTO (Channel channel);
+
+    default int getNumberOfPosts(List<Post> posts) {
+        return posts.size();
+    }
+
 }
