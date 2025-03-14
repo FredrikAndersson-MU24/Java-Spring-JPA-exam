312552b9b6ef3edb1f09c71b582a370b5c3c3881
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-03-14 15:26:18 +0100

    Adapt to new DTOs

--- src/main/java/com/example/javaspringjpaexam/mapper/UserMapper.java
+++ src/main/java/com/example/javaspringjpaexam/mapper/UserMapper.java
@@ -1,15 +1,35 @@
 package com.example.javaspringjpaexam.mapper;
 
-import com.example.javaspringjpaexam.dto.UserDTO;
+import com.example.javaspringjpaexam.dto.UserCreationDTO;
+import com.example.javaspringjpaexam.dto.UserDetailedDTO;
+import com.example.javaspringjpaexam.dto.UserMinimalDTO;
+import com.example.javaspringjpaexam.entity.Post;
 import com.example.javaspringjpaexam.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
+
+import java.util.List;
 
 @Mapper
 public interface UserMapper {
 
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
-    UserDTO userToUserDTO(User user);
+    @Mapping(source = "user.id", target ="id")
+    UserMinimalDTO userToUserMinimalDTO(User user);
+
+    @Mapping(expression = "java(getNumberOfPosts(user))", target = "postCount")
+    UserDetailedDTO userToUserDetailedDTO(User user);
+
+    default int getNumberOfPosts(User user) {
+        List<Post> posts = user.getPosts();
+        if (posts != null) {
+            return posts.size();
+        }
+        return 0;
+    }
+
+    User userCreationDTOToUser (UserCreationDTO userCreationDTO);
 
 }
