53a5b4478ae94913a698a53d05f407e9e066d8d3
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-03-14 15:25:44 +0100

    Add queries

--- src/main/java/com/example/javaspringjpaexam/repository/UserRepository.java
+++ src/main/java/com/example/javaspringjpaexam/repository/UserRepository.java
@@ -1,6 +1,9 @@
 package com.example.javaspringjpaexam.repository;
 
 import com.example.javaspringjpaexam.entity.User;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.mapstruct.Mapping;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,10 +14,12 @@ import java.util.List;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    @Mapping(source ="user.id", target ="id")
     List<User> findUsersByUsernameContainingIgnoreCase(String username);
 
     @Query("SELECT u FROM User u WHERE u.firstName LIKE :query OR u.lastName LIKE :query OR u.username LIKE :query")
     List<User> findUsers(@Param("query") String query);
 
 
+    boolean existsByUsername(@NotNull @Size(max = 32, message ="Username can be max 32 chars") String username);
 }
