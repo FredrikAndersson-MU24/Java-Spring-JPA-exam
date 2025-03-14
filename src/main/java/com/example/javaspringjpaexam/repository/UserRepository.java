916258eeeed4c19771df48742c78d5690acd6ce3
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-03-14 13:42:42 +0100

    Add query to search username, firstName and lastName

--- src/main/java/com/example/javaspringjpaexam/repository/UserRepository.java
+++ src/main/java/com/example/javaspringjpaexam/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.example.javaspringjpaexam.repository;
 
 import com.example.javaspringjpaexam.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +11,10 @@ import java.util.List;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    List<User> findUsersByNameContainingIgnoreCase(String name);
+    List<User> findUsersByUsernameContainingIgnoreCase(String username);
+
+    @Query("SELECT u FROM User u WHERE u.firstName LIKE :query OR u.lastName LIKE :query OR u.username LIKE :query")
+    List<User> findUsers(@Param("query") String query);
+
 
 }
