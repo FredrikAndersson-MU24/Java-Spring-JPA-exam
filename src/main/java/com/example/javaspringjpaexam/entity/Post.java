512938a64f88ca1664218475c1e8dca6c9df3107
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-03-15 11:37:21 +0100

    Set min body 6 chars

--- src/main/java/com/example/javaspringjpaexam/entity/Post.java
+++ src/main/java/com/example/javaspringjpaexam/entity/Post.java
@@ -22,7 +22,7 @@ public class Post {
     @Size(min = 6, max = 32, message = "The post title must be 6-32 characters.")
     private String title;
     @NotNull
-    @Size(min = 4, max = 160, message = "The body of the post must be 4-160 characters.")
+    @Size(min = 6, max = 160, message = "The body of the post must be 6-160 characters.")
     private String body;
     @CreationTimestamp
     private LocalDate posted;
