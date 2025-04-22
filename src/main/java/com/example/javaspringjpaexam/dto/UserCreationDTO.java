e6ce7a9397f1b79e6af2c863ed13de7c5922cbd2
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-04-22 14:52:26 +0200

    add  constraints

--- src/main/java/com/example/javaspringjpaexam/dto/UserCreationDTO.java
+++ src/main/java/com/example/javaspringjpaexam/dto/UserCreationDTO.java
@@ -1,9 +1,18 @@
 package com.example.javaspringjpaexam.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class UserCreationDTO {
 
+    @NotBlank(message = "must not be blank or null")
+    @Size(max = 32, message ="Username can be max 32 chars")
     private String username;
+    @NotBlank(message = "must not be blank or null")
+    @Size(max = 32, message ="firstName can be max 32 chars")
     private String firstName;
+    @NotBlank(message = "must not be blank or null")
+    @Size(max = 32, message ="lastName can be max 32 chars")
     private String lastName;
 
     public UserCreationDTO(String username, String firstName, String lastName) {
